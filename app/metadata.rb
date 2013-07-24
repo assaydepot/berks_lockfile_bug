2a4565ef61b9aa21ac5269501ede5ff045df9009
Xavier Lange <xrlange@gmail.com>
2013-07-24 15:43:40 -0700

    Bringing in a path-directive

---
+++ app/metadata.rb
@@ -0,0 +1,8 @@
+name             'app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
